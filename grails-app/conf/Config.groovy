cf412fe728813f82829dd7b59d096d50eb5cd0c5
Dean Del Ponte <dean.delponte@gmail.com>
2009-07-15 12:22:39 -0500

    Fixed app-engine deploy error regarding Log4j

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -54,7 +54,7 @@ log4j = {
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
-
+    'null' name:'stacktrace'
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
 	       'org.codehaus.groovy.grails.web.pages', //  GSP
 	       'org.codehaus.groovy.grails.web.sitemesh', //  layouts
