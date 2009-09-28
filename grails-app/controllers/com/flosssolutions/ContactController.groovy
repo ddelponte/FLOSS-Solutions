e9d7a93d66b6f7a507d7ae683d61bf61bf2edae1
Dean Del Ponte <dean.delponte@gmail.com>
2009-09-28 13:53:11 -0500

    Refactoring mail service

--- grails-app/controllers/com/flosssolutions/ContactController.groovy
+++ grails-app/controllers/com/flosssolutions/ContactController.groovy
@@ -1,11 +1,25 @@
 package com.flosssolutions
 
 class ContactController {
+    def mailService
 
     def index = { }
     
     def sendEmail = {
+        mailService.sendMail {
+            to "dean.delponte@flosssolutions.com"
+            from "dean.delponte@flosssolutions.com"
+            subject "[Contact Inquiry]"
+            body '${buildMessageText()}'
+        }
 //        mailService.sendMail(params.name, params.email, params.message)
-//        redirect(controller: "contact", action: "index")
+        redirect(controller: "contact", action: "index")
+    }
+
+    def buildMessageText() {
+        def text = ("Name:  ${params.name}\n" ?: '\n')
+        text = text + ("Email:  ${params.email}\n\n" ?: '\n\n')
+        text = (text + params.message ?: '')
+        return text
     }
 }
